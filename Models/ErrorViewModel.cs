c4e6f9b657fe9c4c685e6a96a4ba8cfb1123f21e
marimoisreal <lolkeklol626@gmail.com>
2025-11-28 08:23:34 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EP._6._2A_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
