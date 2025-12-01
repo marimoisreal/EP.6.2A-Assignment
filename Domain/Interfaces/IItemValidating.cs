8d20f7ed3cabf4a5072d44808c940d7009b099f3
marimoisreal <lolkeklol626@gmail.com>
2025-12-01 23:37:56 +0100

    models and interface validation

---
+++ Domain/Interfaces/IItemValidating.cs
@@ -0,0 +1,9 @@
+﻿namespace EP._6._2A_Assignment.Interfaces
+{
+    public interface IItemValidating
+    {    
+        List<string> GetValidators();
+        string GetCardPartial();
+        
+    }
+}
---
+++ Models/Restaurant.cs
@@ -0,0 +1,29 @@
+﻿using EP._6._2A_Assignment.Interfaces;
+
+namespace EP._6._2A_Assignment.Models
+{
+    public class Restaurant : IItemValidating
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ownerEmailAdress {  get; set; }
+
+        public string Status { get; set; }
+
+        public List<string> GetValidators()
+        {
+            return new List<string>
+            {
+
+                "admin@site@.com"
+
+            };
+        }
+        public string GetCardPartial()
+        {
+            return "_RestaurantCard";
+        }
+    }
+}
